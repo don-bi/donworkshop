208a11e2378976b9666a43346936b3fe1ef535bd
don-bi <dbi30@stuy.edu>
2022-09-22 13:52:51 -0400

    init

---
+++ 04_choose/krewes.py
@@ -0,0 +1,12 @@
+'''
+TLOTFP1 Henry Bach, Donald Bi, Yat Long Chan
+SoftDev
+K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+2022-09-22
+time spent: <elapsed time in hours, rounded to nearest tenth>
+DISCO:
+QCC:
+OPS SUMMARY: Randomly choose between 2, 7, and 8 to 
+'''
+
+def chooseDevo(dict):

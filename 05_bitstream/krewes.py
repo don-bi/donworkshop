74d4377b35f965bb7a6a182ced4b62d2df7ad1d7
don-bi <dbi30@stuy.edu>
2022-09-29 13:59:27 -0400

    finished reading in file, getting devo, and printing their data

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,45 +1,43 @@
 '''
 Donald Bi, David Deng
 SoftDev
-K05 -- sumamry
+K05 -- read in file get data wow    
 2022-09-28
-time spent: 
+time spent: 0.5hr
 DISCO: 
-
+-f String are OP!!!
+-open() to open the file and then .read() to read the file
 QCC: none
-OPS SUMMARY: 
+OPS SUMMARY: We will create a function to read the krewes.txt file and split each devo into their own <key, dictionary> pair 
+Using this dictionary, we will create a function to get the name of a random devo and use that name to get their information.
 '''
 import random as rand
 
-def createDict():
-    f = open('krewes.txt')
-    devo_list = f.read().split("@@@")
-    devo_dict = {}
+def create_dict():
+    f = open('krewes.txt') #opens krewes.txt
+    devo_list = f.read().split("@@@") #creates list of devos
+    devo_dict = {} #final return variable
     for devo in devo_list:
-        data = devo.split("$$$")
-    print(devolist)
+        data = devo.split("$$$")#creates a list of each devo's data
+        devo_dict[data[1]] = {"period":data[0],"ducky":data[2]} #name as key assigned to dictionary of devo data
+    #print(devo_dict)
+    return devo_dict
 
-createDict()
+#creates initial dictionary of devo data
+devos = create_dict()
 
-#chooses num devos and returns them as a dictionary
-def chooseNDevos(d,num):
-    newD = {}
-    for i in range(num):
-        (period, devo) = chooseDevo(krewes)
-        if period not in newD:
-            newD[period] = [devo]
-        else:
-            newD[period].append(devo)
-    return newD
+#returns the name of a random devo
+def choose_devo():
+    key_list = list(devos)
+    key = rand.choice(key_list)
+    return key
 
-#returns a list of a random devo and their period
-def chooseDevo(d):
-    pds = list(d)
-    key = rand.choice(pds)
-    devoList = d[key]
-    return [key, rand.choice(devoList)]
+#returns a string of a devo's name, period, and ducky
+def get_devo_data(devo):
+    period = devos[devo]["period"]
+    ducky = devos[devo]["ducky"]
+    return f"{devo} [period: {period}, ducky: {ducky}]"
 
-#prints out period and devo
-def printDevo(d):
-    (period, devo) = chooseDevo(krewes)
-    print((str)(period) + ": " + devo)
+rand_devo = choose_devo()
+print(rand_devo)
+print(get_devo_data(rand_devo))

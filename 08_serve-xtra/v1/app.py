4d546239ae0f7e847f285d064d5f0afee4624096
don-bi <dbi30@stuy.edu>
2022-10-11 13:54:56 -0400

    copied over 09 files

---
+++ 08_serve-xtra/v1/app.py
@@ -0,0 +1,14 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2022
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def hello_world():
+    return "No hablo queso!"
+
+app.run()
+
+#It will just create a blank page with "No hablo queso!" GET favicon.ico & HTTP/1.1 

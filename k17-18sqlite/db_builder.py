516d8c08d66538b64f60b49be5279de0a723bca3
don-bi <dbi30@stuy.edu>
2022-10-26 13:23:56 -0400

    discods qccs

--- k17-18sqlite/db_builder.py
+++ k17-18sqlite/db_builder.py
@@ -1,7 +1,20 @@
-#RICE EXPLOSION :: Nakib Abedin, Wilson Mach, Donald Bi
-#SoftDev  
-#k18:: SQLITE3 BASICS
-#2022-10-25
+'''
+RICE EXPLOSION :: Nakib Abedin, Wilson Mach, Donald Bi
+SoftDev  
+k18:: SQLITE3 BASICS
+Time Spent: 1.5 hrs
+2022-10-25
+
+DISCO:
+-fstrings can help add data into the table without concatenation
+-drop table helps solve the issue with the table already existing
+-learned how to use sqlite3 in python file
+-we can use fetchall() to print the tables in python
+-we can use open() with DictReader to go through a csv file
+
+QCC:
+-what exactly does fetchall() do to allow us to print it?
+'''
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
